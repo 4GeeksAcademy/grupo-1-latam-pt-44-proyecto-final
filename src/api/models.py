cd4bb4b03ea71729448474e1a98675065ad0a7cd
Leonardo Rospigliosi <70114762+LeonardoRospigliosi@users.noreply.github.com>
2025-04-24 02:52:15 +0000

    commiteado

--- src/api/models.py
+++ src/api/models.py
@@ -9,6 +9,7 @@ class User(db.Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
@@ -18,6 +19,7 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
             "favoritos": [f.serialize() for f in self.favoritos]
         }
 
